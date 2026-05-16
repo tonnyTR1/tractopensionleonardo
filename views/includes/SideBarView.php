35435642c92a07bc51642b7f49ae2f71509f52f9
Angel Jesus Morales Guzman <estrellas0272@gmail.com>
2026-05-15 23:17:41 -0600

    Cambio de iconos

--- views/includes/SideBarView.php
+++ views/includes/SideBarView.php
@@ -4,51 +4,66 @@ function SideBarView(){
      <nav class='sidebar sidebar-offcanvas' id='sidebar'>
           <ul class='nav'>
             <li class='nav-item nav-category'>Menú</li>
+
             <li class='nav-item'>
               <a class='nav-link' href='?menu=panel'>
-                <span class='icon-bg'><i class='mdi mdi-cube menu-icon'></i></span>
+                <span class='icon-bg'>
+                  <i class='mdi mdi-view-dashboard menu-icon'></i>
+                </span>
                 <span class='menu-title'>Dashboard</span>
               </a>
             </li>
             
             <li class='nav-item'>
               <a class='nav-link' href='?menu=micamion'>
-                <span class='icon-bg'><i class='mdi mdi-widgets menu-icon'></i></span>
+                <span class='icon-bg'>
+                  <i class='mdi mdi-truck menu-icon'></i>
+                </span>
                 <span class='menu-title'>Mi camión</span>
               </a>
             </li>
 
             <li class='nav-item'>
               <a class='nav-link' href='?menu=miespacio'>
-                <span class='icon-bg'><i class='mdi mdi-widgets menu-icon'></i></span>
+                <span class='icon-bg'>
+                  <i class='mdi mdi-parking menu-icon'></i>
+                </span>
                 <span class='menu-title'>Mi espacio</span>
               </a>
             </li>
 
             <li class='nav-item'>
               <a class='nav-link' href='?menu=apartarespacio'>
-                <span class='icon-bg'><i class='mdi mdi-widgets menu-icon'></i></span>
+                <span class='icon-bg'>
+                  <i class='mdi mdi-map-marker-plus menu-icon'></i>
+                </span>
                 <span class='menu-title'>Apartar Espacio</span>
               </a>
             </li>
 
             <li class='nav-item'>
               <a class='nav-link' href='?menu=pagos'>
-                <span class='icon-bg'><i class='mdi mdi-widgets menu-icon'></i></span>
+                <span class='icon-bg'>
+                  <i class='mdi mdi-cash-multiple menu-icon'></i>
+                </span>
                 <span class='menu-title'>Pagos</span>
               </a>
             </li>
 
             <li class='nav-item'>
               <a class='nav-link' href='?menu=historial'>
-                <span class='icon-bg'><i class='mdi mdi-widgets menu-icon'></i></span>
+                <span class='icon-bg'>
+                  <i class='mdi mdi-history menu-icon'></i>
+                </span>
                 <span class='menu-title'>Historial</span>
               </a>
             </li>
 
             <li class='nav-item'>
               <a class='nav-link' href='?menu=accesos'>
-                <span class='icon-bg'><i class='mdi mdi-widgets menu-icon'></i></span>
+                <span class='icon-bg'>
+                  <i class='mdi mdi-door-open menu-icon'></i>
+                </span>
                 <span class='menu-title'>Accesos</span>
               </a>
             </li>
